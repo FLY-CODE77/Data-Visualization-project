f836ac99b7cf763200c61dd31a372842e3ba36f5
FLY-CODE77 <lightyear94122@gmail.com>
2021-07-11 23:01:04 +0900

    DataFrame making

--- race-bar-animation/main.py
+++ race-bar-animation/main.py
@@ -1,3 +1,20 @@
 import pandas as pd 
 import matplotlib.pylab as plt
 import numpy as np
+
+df = pd.read_csv('./Data/data.csv', sep='\t', engine="python")
+# make score columns
+# when 1 rank points 100 score 100 rank application points 0 score
+df["score"] = 101- df["rank"]
+
+# data cleaning
+df["yearID"] = df["yearID"].str.replace("Y","").astype(int)
+df["num"] = df["yearID"] * df['weekIndex']
+df["num"] = df["yearID"] * df['weekIndex']
+
+# Data frame making
+data = pd.DataFrame(index= list(df["appID"].unique())).reset_index().rename(columns={"index": "appID"})
+
+for i in range(int(len(df)/100)):
+    df1 = df.iloc[i*100: (i+1)*100]
+    data = pd.merge(data, df1, on="appID", how="outer").fillna(0)
