4d6079fcc227f9a0d1ba1538e2d5303a8c68bbc3
FLY-CODE77 <lightyear94122@gmail.com>
2021-07-11 23:01:56 +0900

    Data Cleaning2 for bar-race-chart

--- race-bar-animation/main.py
+++ race-bar-animation/main.py
@@ -12,9 +12,17 @@ df["yearID"] = df["yearID"].str.replace("Y","").astype(int)
 df["num"] = df["yearID"] * df['weekIndex']
 df["num"] = df["yearID"] * df['weekIndex']
 
-# Data frame making
+# Dataframe making
 data = pd.DataFrame(index= list(df["appID"].unique())).reset_index().rename(columns={"index": "appID"})
 
 for i in range(int(len(df)/100)):
     df1 = df.iloc[i*100: (i+1)*100]
     data = pd.merge(data, df1, on="appID", how="outer").fillna(0)
+
+# Data cleaning2 for animation
+data = data.drop_duplicates("appID")
+data = data.T
+data = data.rename(columns = data.iloc[0])
+data = data.drop(data.index[0])
+data = data.reset_index(drop=True).astype(int)
+data.index = (data.index +1).map(str) + "week"
